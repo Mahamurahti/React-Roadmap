672da4a06b2fc354f9720df9da82726ea13df7c4
Eric <55787260+Mahamurahti@users.noreply.github.com>
2021-05-20 12:54:38 +0300

    Hooks exercise 1 done

---
+++ React/hooks/README.md
@@ -0,0 +1,3 @@
+# Hooks
+In the "src" folder there is index.js file. In that file only the rendered component needs to be changed into the 
+correct one so that the correct exercise is displayed.
---
+++ React/hooks/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IntroducingHooks from './IntroducingHooks';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <IntroducingHooks />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
